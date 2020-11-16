f677ab68838f3240c2fb70808cf92f2368371213
Xworkz-kavithab <bkavithashree23@gmail.com>
2020-11-16 22:37:31 +0530

    adding method java files

---
+++ method/Malls.java
@@ -0,0 +1,16 @@
+class Malls
+{
+  static void gtMall()
+  {
+  System.out.println("Good");
+  }
+  static void orionMall()
+  {
+  System.out.println("Average");
+  }
+  static void mantriSquareMall()
+  {
+  System.out.println("Low");
+  }
+ }
+ 
---
+++ method/Method.java
@@ -0,0 +1,25 @@
+ class Gadgets
+ {
+    public static void main(String a[])
+	{
+	watches();
+	smartPhones();
+	headPhones();
+ 
+	}
+ 
+ 
+    static void watches()
+	{
+	System.out.println("sonata");
+	}
+	static void smartPhones()
+	{
+	System.out.println("Mi");
+	}
+	static void headPhones()
+	{
+	System.out.println("boat");
+	}
+ }
+ 
