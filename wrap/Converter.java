539c0348aaef056256619cc3d6d5fade17b9f3ab
Xworkz-kavithab <bkavithashree23@gmail.com>
2020-11-16 23:26:27 +0530

    adding Wrai java files

---
+++ wrap/Converter.java
@@ -0,0 +1,13 @@
+class ConvertPriToNonPri
+{
+ public static void main(String dd[])
+{
+ Integer     myInt=  456;
+ Long myLong=  5234567777l;
+ Character myChar ='a';
+ System.out.println(myInt);
+ System.out.println(myLong);
+ System.out.println(myChar);
+ }
+ }
+ 
---
+++ wrap/Wrapparse.java
@@ -0,0 +1,17 @@
+class  UserInfo
+{
+  public static void main(String ss[])
+{
+  for (int info=0;info<=ss.length-1;info++)
+{
+  int userinfo=Integer.parseInt(ss[info]);
+  System.out.println(userinfo);
+}
+}
+}
+
+
+
+       
+
+
