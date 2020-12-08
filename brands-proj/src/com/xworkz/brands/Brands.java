4a2035b704f5d4c1094c3f740f956ee920d02f82
Xworkz-kavithab <bkavithashree23@gmail.com>
2020-12-08 09:36:41 +0530

    adding files

---
+++ brands-proj/src/com/xworkz/brands/Brands.java
@@ -0,0 +1,21 @@
+package com.xworkz.brands;
+
+public class Brands {
+   private  String name;
+     
+     
+     public Brands() {
+		
+	} 
+     
+     public String getName()
+     {
+    	 return name;
+    	 
+     }
+     public void setName(String name)
+     {
+    	 this.name=name;
+    	 
+     }
+}
---
+++ brands-proj/src/com/xworkz/util/BrandsUtil.java
@@ -0,0 +1,15 @@
+package com.xworkz.util;
+
+import com.xworkz.brands.Brands;
+
+public class BrandsUtil {
+
+	
+	public static void main(String[] args) {
+		
+		Brands b=new Brands();
+		b.setName("Roadster");
+		System.out.println(b.getName());
+		
+	}
+}
